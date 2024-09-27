37eff28cdc235a47f3d4c9b76bb74a5b4453db19
sandeep-luhaniwal <sandeepluh0001@gmail.com>
2024-09-27 22:52:13 +0530

    again done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import Referee from './components/Referee/Referee';
 
 function App() {
   return (
-    <div className='h-[700px] overflow-hidden'>
-      <div id="app" className='bg-emerald-900 flex flex-col items-center justify-center  relative'>
-        <Referee />
-      </div>
+    <div id="app" className='bg-emerald-900 flex flex-col items-center justify-center min-h-screen relative'>
+      <Referee />
     </div>
   );
 }
